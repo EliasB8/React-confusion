1b09092dfa6b724924beaee43cc72c2660f2f2c4
Elias Ferede <feredeelias@gmail.com>
2020-07-28 14:56:09 -0700

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,64 +1,60 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 import dateFormat from "dateformat";
 
-class Dishdetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
-    renderDish(dish) {
+
+function RenderDish({ dish }) {
+    return (
+        <Card>
+            <CardImg width="100%" object src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle heading>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+function RenderComments({ comments }) {
+    if (comments) {
+        const dishComments = comments.map(foundComment => {
+            const dateFormatted = dateFormat(foundComment.date, "mediumDate");
+            return (
+                <li>
+                    <p>{foundComment.comment}</p>
+                    <p>-- {foundComment.author}, {dateFormatted}</p>
+                </li>
+            )
+        });
         return (
-            <Card>
-                <CardImg width="100%" object src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle heading>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {dishComments}
+                </ul>
+            </div>
         );
+    } else {
+        return <div></div>
     }
+}
 
-    renderComments(comments) {
-        if (comments) {
-            const dishComments = comments.map(foundComment => {
-                const dateFormatted = dateFormat(foundComment.date, "mediumDate");
-                return (
-                    <li>
-                        <p>{foundComment.comment}</p>
-                        <p>-- {foundComment.author}, {dateFormatted}</p>
-                    </li>
-                )
-            });
-            return (
-                <div>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {dishComments}
-                    </ul>
-                </div>
-            );
-        } else {
-            return <div></div>
-        }
-    }
-
-    render() {
-        if (this.props.dish) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
-                        </div>
+const Dishdetail = (props) => {
+    if (props.dish) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
-                </div>)
-        } else {
-            return <div></div>;
-        }
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.dish.comments} />
+                    </div>
+                </div>
+            </div>)
+    } else {
+        return <div></div>;
     }
 }
 
