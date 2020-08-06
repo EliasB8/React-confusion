bff2a4b8cba6086cdbf1647ef3f6718b843e2acc
Elias Ferede <feredeelias@gmail.com>
2020-08-06 12:50:39 -0700

    Intro to Redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,17 @@
+import {
+    createStore
+} from "redux";
+import {
+    Reducer,
+    initialState
+} from "./reducer.js";
+
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        Reducer,
+        initialState
+    );
+
+    return store;
+};
